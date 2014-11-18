42791d041c394271387ea3dfb869880083b2dde4
Natalia Govor <natalia.govor@itechart-group.com>
2014-11-18 14:18:52 +0300

    Improved file reading

--- src/clusterization/core.clj
+++ src/clusterization/core.clj
@@ -3,11 +3,17 @@
   (:require [clojure.java.io :as io]))
 
 
+(defn my-func
+  [line]
+  (vector line))
+
 (defn read-file
   [file-name]
   (with-open [rdr (io/reader file-name)]
-    (doseq [line (line-seq rdr)]
-      (println line))))
+    (doall (map my-func (line-seq rdr)))))
+  ;(with-open [rdr (io/reader file-name)]
+  ;  (doseq [line (line-seq rdr)]
+  ;    (println line))))
 
 (defn -main
   "I don't do a whole lot ... yet."
