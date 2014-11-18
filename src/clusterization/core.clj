f11fe63115135d5c260d70937cdbb422c4a4db0c
Natalia Govor <ngovor93@gmail.com>
2014-11-19 00:20:58 +0300

    Added file parsing, started clusterization algorithm.

--- src/clusterization/core.clj
+++ src/clusterization/core.clj
@@ -1,22 +1,46 @@
 (ns clusterization.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as string])
+  (import java.lang.Math))
 
-
-(defn my-func
-  [line]
-  (vector line))
+(defn parse-str
+  [str]
+  (map #(Integer/parseInt %)
+       (drop-last (string/split str #","))))
 
 (defn read-file
   [file-name]
   (with-open [rdr (io/reader file-name)]
-    (doall (map my-func (line-seq rdr)))))
-  ;(with-open [rdr (io/reader file-name)]
-  ;  (doseq [line (line-seq rdr)]
-  ;    (println line))))
+    (doall (map parse-str (line-seq rdr)))))
+
+(defn hamming-distance
+  [x1 x2]
+  (println "hamming"))
+
+(defn euclid-distance
+  [x1 x2]
+  (println "euclid"))
+
+(defn count-potentials
+  [data-point alpha]
+  (println "count potentials"))
+
+(defn clusterize
+  [data-points distance]
+  (let [radius-a 1.5
+        radius-b (* radius-a 1.5)
+        alpha (/ 4 (* radius-a radius-a))
+        beta (/ 4 (* radius-b radius-b))
+        upper-threshold 0.5
+        lower-threshold 0.15
+        potentials (count-potentials data-points alpha)]
+    (println "clusterize")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [file-name & the-rest]
-  (println file-name)
-  (read-file file-name))
+  [& args]
+  (if (>= (count args) 2)
+    (let [data-points (read-file (first args))
+          distance (if (= (last args) "hamming") hamming-distance euclid-distance)]
+      (clusterize data-points distance))
+    (println "fail")))
